a79c9574a20e8d6fbe6a04d1cc93f535f9e142fe
TAHAWY <70866079+tahawy111@users.noreply.github.com>
2022-07-06 18:25:40 +0200

    finish order and cart route

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,73 @@
 import express from "express";
+import Order from "../models/Order.js";
+import {
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+} from "./verifyToken.js";
 const router = express.Router();
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+
+    res.status(200).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete
+
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndRemove(req.params.id);
+    res.status(200).json("Order has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get user orders
+
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get all
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get monthly income
+
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {});
 
 export default router;
